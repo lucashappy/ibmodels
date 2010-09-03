dfe8ad9ab00a99eaba3529fe5d6841cb205972e8
dericksaga <derick_saga@yahoo.com.br>
2010-09-03 11:52:58 -0300

    algumas mudanças no admin

--- components/com_models/assets/scripts.js
+++ components/com_models/assets/scripts.js
@@ -1,38 +1,4 @@
 
-   1. var gal = {
-   2. init : function() {
-   3. if (!document.getElementById || !document.createElement || !document.appendChild) return false;
-   4. if (document.getElementById('gallery')) document.getElementById('gallery').id = 'jgal';
-   5. var li = document.getElementById('jgal').getElementsByTagName('li');
-   6. li[0].className = 'active';
-   7. for (i=0; i<li.length; i++) {
-   8. li[i].style.backgroundImage = 'url(' + li[i].getElementsByTagName('img')[0].src + ')';
-   9. li[i].title = li[i].getElementsByTagName('img')[0].alt;
-  10. gal.addEvent(li[i],'click',function() {
-  11. var im = document.getElementById('jgal').getElementsByTagName('li');
-  12. for (j=0; j<im.length; j++) {
-  13. im[j].className = '';
-  14. }
-  15. this.className = 'active';
-  16. });
-  17. }
-  18. },
-  19. addEvent : function(obj, type, fn) {
-  20. if (obj.addEventListener) {
-  21. obj.addEventListener(type, fn, false);
-  22. }
-  23. else if (obj.attachEvent) {
-  24. obj["e"+type+fn] = fn;
-  25. obj[type+fn] = function() { obj["e"+type+fn]( window.event ); }
-  26. obj.attachEvent("on"+type, obj[type+fn]);
-  27. }
-  28. }
-  29. }
-  30. gal.addEvent(window,'load', function() {
-  31. gal.init();
-  32. });
-
-   
 function showModelInfo(data,url,photosNames){
 	
 	var kwicks = $$('.modelValue');
@@ -80,7 +46,7 @@ function showModelInfo(data,url,photosNames){
 }
 
 function galleryConstruct(){
-	var gall = document.getElementById("galleria");
+	var gall = document.getElementById("#galleria");
 	
 }
 
@@ -88,7 +54,7 @@ function hideModelInfo(){
  
 
 	 //var galeria = $$('#galleria img');
-	 var cell = document.getElementById("galleria");
+	 var cell = document.getElementById("#galleria");
 
 	 if ( cell.hasChildNodes() )
 	 {
